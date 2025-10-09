7702459b6efcf4620fff052783f6b3b630d27111
i-hakob <hakob.injikushyan212@gmail.com>
2025-10-09 16:46:10 -0700

    First 2 homeworks pushed
    The later homeworks will appear here later

---
+++ AI_online_course/homework_1/verify.py
@@ -0,0 +1,9 @@
+import platform
+
+print("Conjragulayshonons! Your local forge is operational.")
+print(f"Python Version: {platform.python_version()}")
+print(f"Operating System: Unfortunately {platform.system()} {platform.release()}")
+
+
+
+#https://colab.research.google.com/drive/1f_XkcJvZ253IYCaeGtm-K-RHy4S7_nuR#scrollTo=RfUbZ034tMU8
